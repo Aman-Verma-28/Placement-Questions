6f322f6728bb1d8a00f88f80af1fcdc54803f8cb
Aman Verma <66964572+Aman-Verma-28@users.noreply.github.com>
2022-10-12 01:16:09 +0530

    Time: 121 ms (5.09%), Space: 14.1 MB (11.73%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,47 @@
+class TrieNode:
+    def __init__(self, val):
+        self.val = val
+        self.child = [None for i in range(26)]
+        self.count = 0
+        self.isEnd = False
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        root = TrieNode('\0')
+        for string in strs:
+            cur = root
+            length = len(string)
+            if length==0:
+                return ''
+            for index in range(len(string)):
+                value = string[index]
+                if cur.child[ord(value)-ord('a')] is None:
+                    temp = TrieNode(value)
+                    cur.child[ord(value)-ord('a')] = temp
+                else:
+                    cur.child[ord(value)-ord('a')].count+=1
+                cur = cur.child[ord(value)-ord('a')]
+            cur.isEnd = True
+        
+        
+        cur = root
+        ans = ''
+        while True:
+            val = ''
+            seen = False
+            for i in range(26):
+                if cur.child[i] and seen:
+                    return ans
+                if cur.child[i]:
+                    val=cur.child[i].val
+                    seen = True
+                    index = i
+            if val=='':
+                return ans
+            ans+=val
+            cur = cur.child[index]
+            if cur.isEnd:
+                return ans
+        return ans
+            
