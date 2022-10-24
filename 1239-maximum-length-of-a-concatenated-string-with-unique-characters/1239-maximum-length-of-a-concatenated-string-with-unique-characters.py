4137f4bf36c010159e07635cc230be45a027e385
Aman Verma <66964572+Aman-Verma-28@users.noreply.github.com>
2022-10-24 17:08:06 +0530

    Time: 143 ms (80.15%), Space: 14 MB (64.77%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -6,8 +6,6 @@ class Solution:
             if index<0:
                 maxLength[0] = max(maxLength[0], curLength[0])
                 return
-            vis_new = [i for i in visited]
-            cur = [curLength[0]]
             string = arr[index]
             if len(string) == len(set(string)):
                 for char in string:
@@ -19,14 +17,17 @@ class Solution:
                         visited[ord(char)-ord('a')]+=1
                     curLength[0]+=len(string)
                     # maxLength[0] = max(maxLength[0], curLength[0])
-                    recursive(index-1, arr, maxLength, curLength, visited)
+                    recursive(index-1, arr, maxLength, curLength, visited)      
+                    curLength[0]-=len(string)
+                    for char in string:
+                        visited[ord(char)-ord('a')]-=1
                 
                         
                 
             
             
             # not take
-            recursive(index-1, arr, maxLength, cur, vis_new)
+            recursive(index-1, arr, maxLength, curLength, visited)
             
             
             
